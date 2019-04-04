8efc2cd95bde731629ba20d47fa660c272c318ea
matherion <gjalt-jorn@behaviorchange.eu>
2019-04-04 16:07:38 +0200

    Fixed bug that caused present IDs to be overwritten

--- R/to_specList.R
+++ R/to_specList.R
@@ -22,10 +22,10 @@ to_specList <- function(x,
       ### They're all specifications; check whether they all have an id
       ids <-
         purrr::map_chr(x, function(element) {
-          if (is.null(x$id)) {
+          if (is.null(element$id)) {
             return("");
           } else {
-            return(x$id);
+            return(element$id);
           }
         });
       if (all(nchar(ids) > 0)) {
@@ -37,7 +37,7 @@ to_specList <- function(x,
                          names=ids,
                          class=c("justifierSpecList", types)));
       } else {
-        emptyIds <- nchar(ids)==0;
+        emptyIds <- which(nchar(ids)==0);
         ids[emptyIds] <-
           paste0("id_", seq(1, sum(nchar(ids)==0)));
         warning("Some elements did not have identifiers set! I set those ",
