6820ca1a2878de2442fa46c81383b4ccd350706e
matherion <gjalt-jorn@behaviorchange.eu>
2019-04-05 19:40:22 +0200

    Exporting sanitize_for_DiagrammeR

--- R/sanitize_for_DiagrammeR.R
+++ R/sanitize_for_DiagrammeR.R
@@ -1,3 +1,16 @@
+#' Sanitize for DiagrammeR
+#'
+#' Basically a wrapper for [base::gsub()] to sanitize a string for
+#' DiagrammeR
+#'
+#' @param x The string or vector
+#' @param regExReplacements A list of two-element character vectors; first element
+#' should be the elemnet to search, and the second element, the replacement.
+#'
+#' @return The sanitized character vector
+#' @export
+#'
+#' @examples justifier::sanitize_for_DiagrammeR("This is or isn't problematic');
 sanitize_for_DiagrammeR <- function(x,
                                     regExReplacements = list(c("\\\"", "`"),
                                                              c("\\'", "`"),
